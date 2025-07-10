86cf9618bbc81ed9b138dc129f11f927efd4b08a
Sanyam Jain <156403166+sanyamkanu@users.noreply.github.com>
2025-07-10 23:36:37 +0530

    backend link

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
 
     try {
       // ✅ Fix: Add http:// in URL
-      const res = await axios.post("https://pdf-word-production.up.railway.app/", formData);
+      const res = await axios.post("https://pdf-word-production.up.railway.app/convert/pdf-to-word", formData);
       setDownloadUrl(res.data.downloadUrl);
     } catch (err) {
       console.error(err);
